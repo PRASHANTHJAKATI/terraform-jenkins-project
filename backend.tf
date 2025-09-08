e9f7e35ec954eeb3e13eb39d9493fa57d5622e26
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-08 13:42:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,4 +1,20 @@
 terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+       version = "6.12.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"  # or your preferred region
+  access_key = "AKIATE2YZM62ORTAZREW"       # optional if using environment variables
+  secret_key = "6U49mZCKHMUXRfM/Uz5pRRHuZV+YqfCr7mHf2aIY"       # optional if using environment variables
+}
+
+terraform {
   backend "s3" {
     bucket = "naresh-aws-test"
     key = "terraform.tfstate"
