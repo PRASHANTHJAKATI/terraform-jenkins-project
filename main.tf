09b91f513619d9329e9cf059ffd6514ce91125df
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-08 15:52:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-       version = "6.12.0"
-    }
-  }
-}
-
-
-provider "aws" {
-  region     = "us-east-1"  # or your preferred region
-  access_key = "AKIATE2YZM62ORTAZREW"       # optional if using environment variables
-  secret_key = "6U49mZCKHMUXRfM/Uz5pRRHuZV+YqfCr7mHf2aIY"       # optional if using environment variables
-}
-
 provider "aws" {
   region = var.aws_region
 }
