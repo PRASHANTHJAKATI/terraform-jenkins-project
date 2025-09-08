f5336c60d7f2d72a55697c4423b21f06d11be371
PRASHANTHJAKATI <166536642+PRASHANTHJAKATI@users.noreply.github.com>
2025-09-08 13:41:25 +0530

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,3 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+       version = "6.12.0"
+    }
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"  # or your preferred region
+  access_key = "AKIATE2YZM62ORTAZREW"       # optional if using environment variables
+  secret_key = "6U49mZCKHMUXRfM/Uz5pRRHuZV+YqfCr7mHf2aIY"       # optional if using environment variables
+}
 resource "aws_s3_bucket" "my-s3-bucket" {
   bucket_prefix = var.bucket_prefix
   acl = var.acl
